86663a6d2a1a46f53179a26365437282460a6e24
Kangear <kangear@163.com>
2016-06-22 13:33:13 +0800

    实现生成PDF DEMO

--- BodyCompositionAnalyzer/src/main/java/com/example/tony/bodycompositionanalyzer/MainActivity.java
+++ BodyCompositionAnalyzer/src/main/java/com/example/tony/bodycompositionanalyzer/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.tony.bodycompositionanalyzer;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.print.PrintAttributes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.tony.bodycompositionanalyzer.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "PDFdemo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +43,69 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+    }
+
+    public void onClick(View v) {
+        // create a new document
+        PdfDocument document = new PdfDocument();
+
+        // crate a page description
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(
+                PrintAttributes.MediaSize.ISO_A4.getWidthMils() * 72 / 1000,
+                PrintAttributes.MediaSize.ISO_A4.getHeightMils() * 72 / 1000, 1)
+                .create();
+
+        // start a page
+        PdfDocument.Page page = document.startPage(pageInfo);
+
+        // 画笔
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        // 写「Hello World」
+        paint.setColor(Color.BLACK);
+        page.getCanvas().drawText("Hello World!", 50, 50, paint);
+
+        // 写「√」
+        paint.setColor(Color.BLACK);
+        page.getCanvas().drawText("√", 100, 100, paint);
+
+
+        // draw something on the page
+        View content = findViewById(android.R.id.content);;
+//       content.draw(page.getCanvas());
+
+        // finish the page
+        document.finishPage(page);
+        // add more pages
+        // write the document content
+        FileOutputStream os = null;
+        String string = getExternalFilesDir(Environment.DIRECTORY_DCIM)
+                + File.separator + "test.pdf";
+        try {
+            Log.i(LOG_TAG, "String:" + string);
+            os = new FileOutputStream(string);
+            document.writeTo(os);
+            os.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            // close the document
+            document.close();
+        }
+
+        startActivity(getPdfFileIntent(string));
+    }
+
+    //android获取一个用于打开PDF文件的intent
+    public static Intent getPdfFileIntent(String param ) {
+        Intent intent = new Intent("android.intent.action.VIEW");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri uri = Uri.fromFile(new File(param));
+        intent.setDataAndType(uri, "application/pdf");
+        return intent;
     }
 
     @Override
