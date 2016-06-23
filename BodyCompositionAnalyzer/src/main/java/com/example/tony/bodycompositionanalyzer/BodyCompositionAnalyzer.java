337d665c184cb073e04ddf77aaa64b264d38fa21
Kangear <kangear@163.com>
2016-06-23 22:41:36 +0800

    BodyCompositionAnalyzer 一次大的更新

---
+++ BodyCompositionAnalyzer/src/main/java/com/example/tony/bodycompositionanalyzer/BodyCompositionAnalyzer.java
@@ -0,0 +1,172 @@
+package com.example.tony.bodycompositionanalyzer;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+
+import android_serialport_api.ComBean;
+import android_serialport_api.SerialHelper;
+import android_serialport_api.SerialPortFinder;
+
+/**
+ * Created by tony on 16-6-23.
+ */
+public class BodyCompositionAnalyzer {
+ 	private static final boolean DEBUG = true;
+	private static final String LOG_TAG = "BodyCompositionAnalyzer";
+	/** */
+	private final SerialHelper serialCtrl       = new SerialControl();;
+	private final SerialPortFinder serialPortFinder = new SerialPortFinder();
+	private String                 serialPort       = null;
+
+	/**
+	 * 需要发送的数据
+	 */
+	public static final byte[] SEND = new byte[]{(byte)0xAA, (byte)0xAB, 0x00, 0x04, (byte)0xDE, 0x00, (byte)0xDD, 0x6A};
+	private static final int BYTE_BUFFER_ALLOCATE = 8192;
+
+	/** 串口相关 */
+	private static final String BAUDRATE_COIN = "9600";
+
+	private class SerialControl extends SerialHelper {
+		@Override
+		protected void onDataReceived(final ComBean ComRecData) {
+			handleRecData(ComRecData);
+		}
+
+		/** 处理接收到串口数据事件 */
+		private void handleRecData(final ComBean ComRecData) {
+			if (DEBUG) Log.i(LOG_TAG, "handleRecData:" + bytesToHex(ComRecData.bRec));
+			if (DEBUG) Log.i(LOG_TAG, "handleRecStri:" + new String(ComRecData.bRec));
+			parseData(ComRecData.bRec);
+
+		}
+	}
+
+	private void parseData(byte[] inputData) {
+		Log.i(LOG_TAG, "parseData");
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+        /* 1. 判断回复是否正常 */
+		byte[] ack = new byte[BodyComposition.ACK_LENGTH];
+		System.arraycopy(inputData, BodyComposition.ACK_START, ack, 0, BodyComposition.ACK_LENGTH);
+		if(Arrays.equals(ack, BodyComposition.ACK)) {
+            /* 提取各个数据 */
+			byte[] data2 = new byte[BodyComposition.DATA_LENGTH];
+			System.arraycopy(inputData, BodyComposition.DATA_START, data2, 0, BodyComposition.DATA_LENGTH);
+            /* 解析数据 */
+			new BodyComposition(data2);
+		}
+	}
+
+	public static String bytesToHex(byte[] in) {
+	    final StringBuilder builder = new StringBuilder();
+	    for(byte b : in) {
+	        builder.append(String.format("%02x ", b));
+	    }
+	    return builder.toString();
+	}
+
+	/**
+	 * 获取帮助信息
+	 */
+	public static final byte[] getDianjiTest() {
+		byte[] byteArray = null;
+		ByteBuffer target = ByteBuffer.allocate(BYTE_BUFFER_ALLOCATE);
+		/* 1. 初始化 */
+		target.put(SEND);
+		target.limit(target.remaining());
+		target.rewind();
+		/* 提取 */
+		byteArray = new byte[BYTE_BUFFER_ALLOCATE - target.remaining()];
+		target.get(byteArray);
+		/* 将byteBuffer清理 */
+		target.clear();
+		return byteArray;
+	}
+
+	private boolean updatePort() {
+		serialPort = "/dev/ttyS2";
+		return true;
+	}
+
+	/** 初始化串口 */
+	private boolean initCoinMachine() {
+		boolean ret = false;
+		/* 如果已经打开状态，强制关闭，以中断诸如循环发送状态 */
+		/* TODO:如果强制关闭会导致下次读取时会有缓存硬币数量 */
+		if (serialCtrl.isOpen()) {
+			try {
+				serialCtrl.stopSend();
+				Thread.sleep(1000);
+				serialCtrl.close();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+
+		/* 1. 打开串口 */
+		if (serialPort == null) {
+			ret = updatePort();
+			if (!ret) {
+				if (DEBUG) Log.w(LOG_TAG, "UpdatePort failure!");
+				return false;
+			}
+		}
+
+		ret = serialCtrl.setPort(serialPort);
+		if (!ret) {
+			if (DEBUG) Log.w(LOG_TAG, "SetPort serialProt failure!");
+			return false;
+		}
+		ret = serialCtrl.setBaudRate(BAUDRATE_COIN);
+		if (!ret) {
+			if (DEBUG) Log.w(LOG_TAG, "setBaudRate serialProt failure!");
+			return false;
+		}
+		try {
+			serialCtrl.open();
+		} catch (SecurityException e) {
+			if (DEBUG) Log.w(LOG_TAG, "打开串口失败:没有串口读/写权限!");
+			return false;
+		} catch (IOException e) {
+			if (DEBUG) Log.w(LOG_TAG, "打开串口失败:未知错误!");
+			return false;
+		} catch (InvalidParameterException e) {
+			if (DEBUG) Log.w(LOG_TAG, "打开串口失败:参数错误!");
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * 打印小票的例子
+	 * @return
+	 */
+	public boolean doPrint() {
+		Log.i(LOG_TAG, "doPrint");
+
+		/* 1. 打开串口 */
+		if (!initCoinMachine()) {
+			if (DEBUG)
+				Log.w(LOG_TAG, "init Thermal Printer fail!");
+			return false;
+		}
+		/* 2. 发送数据 */
+		serialCtrl.send(getDianjiTest());
+
+		/* 3. 关闭串口 */
+		serialCtrl.close();
+		return false;
+	}
+}
--- BodyCompositionAnalyzer/src/main/java/com/example/tony/bodycompositionanalyzer/MainActivity.java
+++ BodyCompositionAnalyzer/src/main/java/com/example/tony/bodycompositionanalyzer/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.tony.bodycompositionanalyzer;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.pdf.PdfDocument;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,14 +12,13 @@ import android.os.Environment;
 import android.print.PrintAttributes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.print.PrintHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.example.tony.bodycompositionanalyzer.R;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -30,6 +30,7 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = "PDFdemo";
+    private static BodyCompositionAnalyzer mBodyCompositionAnalyzer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,80 +48,98 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        mBodyCompositionAnalyzer = new BodyCompositionAnalyzer();
     }
 
+    /**
+     * 当API >= 19 时使用系统自带PDF API，否则使用iText
+     * @return
+     */
     public String createPdf() {
-        // create a new document
-        PdfDocument document = new PdfDocument();
-
-        // crate a page description
-        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(
-                PrintAttributes.MediaSize.ISO_A4.getWidthMils() * 72 / 1000,
-                PrintAttributes.MediaSize.ISO_A4.getHeightMils() * 72 / 1000, 1)
-                .create();
-
-        // start a page
-        PdfDocument.Page page = document.startPage(pageInfo);
-
-        // 画笔
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
-        // 写「Hello World」
-        paint.setColor(Color.BLACK);
-        page.getCanvas().drawText("Hello World!", 50, 50, paint);
-
-        // 写「Hello World」
-        paint.setColor(Color.BLACK);
-        page.getCanvas().drawText("Hello World!", 20, 20, paint);
-
-        // 写「√」
-        paint.setColor(Color.BLACK);
-        page.getCanvas().drawText("√", 100, 100, paint);
-
-
-        // draw something on the page
-        View content = findViewById(android.R.id.content);;
-//       content.draw(page.getCanvas());
-
-        // finish the page
-        document.finishPage(page);
-        // add more pages
-        // write the document content
-        FileOutputStream os = null;
         String string = getExternalFilesDir(Environment.DIRECTORY_DCIM)
                 + File.separator + "test.pdf";
-        try {
-            Log.i(LOG_TAG, "String:" + string);
-            os = new FileOutputStream(string);
-            document.writeTo(os);
-            os.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } finally {
-            // close the document
-            document.close();
-        }
-
+//        // create a new document
+//        PdfDocument document = new PdfDocument();
+//
+//        // crate a page description
+//        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(
+//                PrintAttributes.MediaSize.ISO_A4.getWidthMils() * 72 / 1000,
+//                PrintAttributes.MediaSize.ISO_A4.getHeightMils() * 72 / 1000, 1)
+//                .create();
+//
+//        // start a page
+//        PdfDocument.Page page = document.startPage(pageInfo);
+//
+//        // 画笔
+//        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+//
+//        // 写「Hello World」
+//        paint.setColor(Color.BLACK);
+//        page.getCanvas().drawText("Hello World!", 50, 50, paint);
+//
+//        // 写「Hello World」
+//        paint.setColor(Color.BLACK);
+//        page.getCanvas().drawText("Hello World!", 20, 20, paint);
+//
+//        // 写「√」
+//        paint.setColor(Color.BLACK);
+//        page.getCanvas().drawText("√", 100, 100, paint);
+//
+//
+//        // draw something on the page
+//        View content = findViewById(android.R.id.content);;
+////       content.draw(page.getCanvas());
+//
+//        // finish the page
+//        document.finishPage(page);
+//        // add more pages
+//        // write the document content
+//        FileOutputStream os = null;
+//        try {
+//            Log.i(LOG_TAG, "String:" + string);
+//            os = new FileOutputStream(string);
+//            document.writeTo(os);
+//            os.close();
+//        } catch (IOException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        } finally {
+//            // close the document
+//            document.close();
+//        }
+//
         return string;
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.parse_textview:
-                try {
-                    parseData();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            case R.id.parse_button:
+                mBodyCompositionAnalyzer.doPrint();
                 break;
             case R.id.textview:
-                /* 创建PDF */
-                String string = createPdf();
-                /* 打开PDF */
-                startActivity(getPdfFileIntent(string));
+//                /* 创建PDF */
+//                String string = createPdf();
+//                /* 打开PDF */
+//                startActivity(getPdfFileIntent(string));
+                break;
+            case R.id.print_button:
+                doPhotoPrint();
                 break;
         }
+    }
+
+    /**
+     * 打印需求规划
+     * 不使用Android打印服务，但是使用Android USB host口
+     * 串口有两种方式实现，一种基于FTDI厂家的实现，一种是传统串口方式
+     */
+    private void doPhotoPrint() {
+        PrintHelper photoPrinter = new PrintHelper(this);
+        photoPrinter.setScaleMode(PrintHelper.SCALE_MODE_FIT);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.selection);
+        photoPrinter.printBitmap("droids.jpg - test print", bitmap);
     }
 
     private void parseData() throws IOException {
