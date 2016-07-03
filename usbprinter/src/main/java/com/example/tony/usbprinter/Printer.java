da60c907d32fc254c84ea446868fb60c17663cce
Kangear <kangear@163.com>
2016-07-03 19:10:09 +0800

    usbprinter: 将usb打印机封装到一个Printer类中

---
+++ usbprinter/src/main/java/com/example/tony/usbprinter/Printer.java
@@ -0,0 +1,282 @@
+package com.example.tony.usbprinter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.AssetManager;
+import android.hardware.usb.UsbAccessory;
+import android.hardware.usb.UsbConstants;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
+import android.hardware.usb.UsbManager;
+import android.hardware.usb.UsbRequest;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedList;
+
+/**
+ * Created by tony on 16-7-3.
+ */
+public class Printer {
+    private final Context mContext;
+    private UsbManager mManager;
+    private UsbDevice mDevice;
+    private UsbDeviceConnection mDeviceConnection;
+    private UsbInterface mInterface;
+    private PrinterDevice mPrinterDevice;
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+
+    private static final String LOG_TAG = "PrinterDevice";
+
+    private UsbEndpoint mEndpointOut;
+    private UsbEndpoint mEndpointIn;
+    private String mSerial;
+    private UsbInterface mUsbInterface = null;
+
+    // pool of requests for the OUT endpoint
+    private final LinkedList<UsbRequest> mOutRequestPool = new LinkedList<UsbRequest>();
+    // pool of requests for the IN endpoint
+    private final LinkedList<UsbRequest> mInRequestPool = new LinkedList<UsbRequest>();
+
+    AssetManager mAssetManager = null;
+    InputStream mInputStream = null;
+    public Printer(Context context) {
+        Log.i(LOG_TAG, "Printer");
+        mContext = context;
+
+        /* 只处理检测到的第一个打印机，其它不进行处理 */
+        mManager = (UsbManager) mContext.getSystemService(Context.USB_SERVICE);
+        for(UsbDevice device : mManager.getDeviceList().values()) {
+            UsbInterface intf = findPrinterInterface(device);
+            if (setPrinterInterface(device, intf)) {
+                initPrinterDevice(mDeviceConnection, intf);
+                mUsbInterface = intf;
+                break;
+            }
+        }
+
+        // listen for new devices
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        filter.addAction(ACTION_USB_PERMISSION);
+        mContext.registerReceiver(mUsbReceiver, filter);
+
+    }
+
+    /**
+     * 判断是否连接上打印机
+     * @return
+     */
+    public boolean isConnected() {
+        return mUsbInterface == null ? false : true;
+    }
+
+    /**
+     * 获取打印机型号
+     */
+    public String getModel() {
+        return "HP Deskjet 1112";
+    }
+
+    BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
+                UsbDevice device = (UsbDevice) intent
+                        .getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                UsbInterface intf = findPrinterInterface(device);
+                if (intf != null) {
+                    Log.i(LOG_TAG, "Found Printer interface " + intf);
+                    setPrinterInterface(device, intf);
+                }
+            } else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+                UsbDevice device = intent
+                        .getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                String deviceName = device.getDeviceName();
+                if (mDevice != null && mDevice.equals(deviceName)) {
+                    Log.i(LOG_TAG, "printer interface removed");
+                    setPrinterInterface(null, null);
+                }
+            } else if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbAccessory accessory = (UsbAccessory) intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
+
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if(accessory != null){
+                            //call method to set up accessory communication
+                        }
+                    }
+                    else {
+                        Log.d(LOG_TAG, "permission denied for accessory " + accessory);
+                    }
+                }
+            }
+        }
+    };
+
+    // searches for an adb interface on the given USB device
+    static private UsbInterface findPrinterInterface(UsbDevice device) {
+        Log.d(LOG_TAG, "findPrinterInterface " + device);
+        int count = device.getInterfaceCount();
+        for (int i = 0; i < count; i++) {
+            UsbInterface intf = device.getInterface(i);
+            if (intf.getInterfaceClass() == UsbConstants.USB_CLASS_PRINTER) {
+                Log.i(LOG_TAG, "Printer");
+                return intf;
+            }
+        }
+        return null;
+    }
+
+
+    // Sets the current USB device and interface
+    private boolean setPrinterInterface(UsbDevice device, UsbInterface intf) {
+        if (mDeviceConnection != null) {
+            if (mInterface != null) {
+                mDeviceConnection.releaseInterface(mInterface);
+                mInterface = null;
+            }
+            mDeviceConnection.close();
+            mDevice = null;
+            mDeviceConnection = null;
+        }
+
+        if (device != null && intf != null) {
+            UsbDeviceConnection connection = mManager.openDevice(device);
+            if (connection != null) {
+                Log.i(LOG_TAG, "open succeeded");
+                if (connection.claimInterface(intf, false)) {
+                    Log.i(LOG_TAG, "claim interface succeeded");
+                    mDevice = device;
+                    mDeviceConnection = connection;
+                    mInterface = intf;
+//                    mPrinterDevice = new PrinterDevice(mContext, mDeviceConnection, intf);
+//                    mPrinterDevice.write(mInputStream);
+//                    Log.i(LOG_TAG, "call start");
+//                    mPrinterDevice.start();
+                    return true;
+                } else {
+                    Log.i(LOG_TAG, "claim interface failed");
+                    connection.close();
+                }
+            } else {
+                Log.i(LOG_TAG, "open failed");
+            }
+        }
+
+        if (mDeviceConnection == null && mPrinterDevice != null) {
+            //mPrinterDevice.stop();
+            mPrinterDevice = null;
+        }
+        return false;
+    }
+
+    /**
+     * 获取打印机型号等信息
+     * @param connection UsbDeviceConnection
+     * @param intf UsbInterface
+     */
+    private void initPrinterDevice(UsbDeviceConnection connection,
+                         UsbInterface intf) {
+        mDeviceConnection = connection;
+        mSerial = connection.getSerial();
+
+        UsbEndpoint epOut = null;
+        UsbEndpoint epIn = null;
+        // look for our bulk endpoints
+        for (int i = 0; i < intf.getEndpointCount(); i++) {
+            UsbEndpoint ep = intf.getEndpoint(i);
+            if (ep.getType() == UsbConstants.USB_ENDPOINT_XFER_BULK) {
+                if (ep.getDirection() == UsbConstants.USB_DIR_OUT) {
+                    epOut = ep;
+                } else {
+                    epIn = ep;
+                }
+            }
+        }
+        if (epOut == null || epIn == null) {
+            throw new IllegalArgumentException("not all endpoints found");
+        }
+        mEndpointOut = epOut;
+        mEndpointIn = epIn;
+    }
+
+    /**
+     * 写数据
+     * @param in
+     */
+    public void write(InputStream in) {
+        Runnable r = new WaiterThread(in);
+        new Thread(r).start();
+    }
+
+    /**
+     * 写线程
+     */
+    private class WaiterThread implements Runnable {
+        final InputStream inputStream;
+        public WaiterThread(InputStream in) {
+            inputStream = in;
+        }
+        @Override
+        public void run() {
+            BufferedInputStream bufferInputStream = null;
+
+            try {
+                final int MAX_USBFS_BUFFER_SIZE = 16384; //16KiB.
+                //File pcl3gui = new File("/sdcard/HelloWorld.PCL3GUI");
+                byte[] bytes = new byte[MAX_USBFS_BUFFER_SIZE];
+                bufferInputStream = new BufferedInputStream(inputStream);
+                int bytesRead = 0;
+                int bytesWrite = 0;
+                //从文件中按字节读取内容，到文件尾部时read方法将返回-1
+                while ((bytesRead = bufferInputStream.read(bytes)) != -1) {
+
+                    //将读取的字节转为字符串对象
+                    //String chunk = new String(bytes, 0, bytesRead);
+                    //Log.i("LOG_TAG", chunk);
+
+                    bytesWrite = mDeviceConnection.bulkTransfer(mEndpointOut, bytes,
+                            bytesRead, 40);
+
+                    Log.i(LOG_TAG, "bytesRead:" + bytesRead + " bytesWrite:" + bytesWrite);
+
+                    if(bytesWrite < 0) {
+                        // 暂停此线程 排除故障 暂时做到这种程度，因为对Java多线程编程不是太熟悉。
+                        // 打印被暂停时会进入到这个里面，可能引起的问题有：缺纸 卡纸等等。这个时候要有另外一个线程去获取Printer状态。
+                        Log.e(LOG_TAG, "暂停此线程 排除故障!");
+                        synchronized (this) {
+                            wait();
+                            notifyAll();
+                        }
+                    }
+
+                    if(bytesWrite != bytesRead) {
+                        Log.e(LOG_TAG, "bultTransfer error!");
+                        break;
+                    }
+
+                    Thread.sleep(1000);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if(bufferInputStream != null)
+                        bufferInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
--- usbprinter/src/main/java/com/example/tony/usbprinter/PrinterDevice.java
+++ usbprinter/src/main/java/com/example/tony/usbprinter/PrinterDevice.java
@@ -19,10 +19,10 @@ public class PrinterDevice {
 	private static final String LOG_TAG = "PrinterDevice";
 	
 	private final MainActivity mActivity;
+
 	private final UsbDeviceConnection mDeviceConnection;
 	private final UsbEndpoint mEndpointOut;
 	private final UsbEndpoint mEndpointIn;
-
 	private String mSerial;
 
 	private final WaiterThread mWaiterThread = new WaiterThread();
@@ -32,9 +32,15 @@ public class PrinterDevice {
 	// pool of requests for the IN endpoint
 	private final LinkedList<UsbRequest> mInRequestPool = new LinkedList<UsbRequest>();
 
+	/**
+	 * 获取打印机型号等信息
+	 * @param activity MainActivity
+	 * @param connection UsbDeviceConnection
+	 * @param intf UsbInterface
+	 */
 	public PrinterDevice(MainActivity activity, UsbDeviceConnection connection,
 			UsbInterface intf) {
-		mActivity = activity;
+        mActivity = activity;
 		mDeviceConnection = connection;
 		mSerial = connection.getSerial();
 
@@ -58,12 +64,12 @@ public class PrinterDevice {
 		mEndpointIn = epIn;
 	}
 
-	public void start() {
+	public void start(InputStream in) {
 		mWaiterThread.start();
 	}
 
 	private class WaiterThread extends Thread {
-		public void run() {
+		public void run(InputStream in) {
 			BufferedInputStream bufferInputStream = null;
 			
 			try {
@@ -115,4 +121,75 @@ public class PrinterDevice {
 			}
 		}
 	}
+
+    /**
+     * 写数据
+     * @param in
+     */
+    public void write(InputStream in) {
+        Runnable r = new WaiterThread2(in);
+        new Thread(r).start();
+    }
+
+    /**
+     * 写线程
+     */
+    private class WaiterThread2 implements Runnable {
+        final InputStream inputStream;
+        public WaiterThread2(InputStream in) {
+            inputStream = in;
+        }
+        @Override
+        public void run() {
+            BufferedInputStream bufferInputStream = null;
+
+            try {
+                final int MAX_USBFS_BUFFER_SIZE = 16384; //16KiB.
+                //File pcl3gui = new File("/sdcard/HelloWorld.PCL3GUI");
+                byte[] bytes = new byte[MAX_USBFS_BUFFER_SIZE];
+                bufferInputStream = new BufferedInputStream(inputStream);
+                int bytesRead = 0;
+                int bytesWrite = 0;
+                //从文件中按字节读取内容，到文件尾部时read方法将返回-1
+                while ((bytesRead = bufferInputStream.read(bytes)) != -1) {
+
+                    //将读取的字节转为字符串对象
+                    //String chunk = new String(bytes, 0, bytesRead);
+                    //Log.i("LOG_TAG", chunk);
+
+                    bytesWrite = mDeviceConnection.bulkTransfer(mEndpointOut, bytes,
+                            bytesRead, 40);
+
+                    Log.i(LOG_TAG, "bytesRead:" + bytesRead + " bytesWrite:" + bytesWrite);
+
+                    if(bytesWrite < 0) {
+                        // 暂停此线程 排除故障 暂时做到这种程度，因为对Java多线程编程不是太熟悉。
+                        // 打印被暂停时会进入到这个里面，可能引起的问题有：缺纸 卡纸等等。这个时候要有另外一个线程去获取Printer状态。
+                        Log.e(LOG_TAG, "暂停此线程 排除故障!");
+                        synchronized (this) {
+                            wait();
+                            notifyAll();
+                        }
+                    }
+
+                    if(bytesWrite != bytesRead) {
+                        Log.e(LOG_TAG, "bultTransfer error!");
+                        break;
+                    }
+
+                    Thread.sleep(1000);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if(bufferInputStream != null)
+                        bufferInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
