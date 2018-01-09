ebd69240c730a22b07917c2090c10ca62eb110bd
kangear <kangear@163.com>
2018-01-09 20:59:04 +0800

    APP: 时间修改ok

--- app/src/main/java/com/kangear/bodycompositionanalyzer/TimeActivity.java
+++ app/src/main/java/com/kangear/bodycompositionanalyzer/TimeActivity.java
@@ -1,23 +1,15 @@
 package com.kangear.bodycompositionanalyzer;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-import static com.kangear.bodycompositionanalyzer.WelcomeActivity.CONST_FINGER_ID;
-import static com.kangear.bodycompositionanalyzer.WelcomeActivity.DEFAULT_WEIGHT;
-import static com.kangear.bodycompositionanalyzer.WelcomeActivity.FORMAT_WEIGHT;
-import static com.kangear.bodycompositionanalyzer.WelcomeActivity.INVALID_FINGER_ID;
-import static com.kangear.bodycompositionanalyzer.WelcomeActivity.REQUEST_CODE_TOUCHID;
-import static com.kangear.bodycompositionanalyzer.WelcomeActivity.WEIGHT_NEW_TEST;
-import static com.kangear.bodycompositionanalyzer.WelcomeActivity.WEIGHT_VIP_TEST;
-import static com.kangear.bodycompositionanalyzer.WelcomeActivity.doVipTest;
-import static com.kangear.bodycompositionanalyzer.WelcomeActivity.startTouchId;
+import java.util.Calendar;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -25,25 +17,17 @@ import static com.kangear.bodycompositionanalyzer.WelcomeActivity.startTouchId;
  */
 public class TimeActivity extends AppCompatActivity {
     private static final String TAG = "TimeActivity";
-    private View startView;
-    private View stopView;
-    private TextView mTextView;
-    private static final int WEIGHT_ACTIVITY = 1;
-    private static final int WEIGHT_STOP = 2;
-    private int bootTag;
+    private DatePicker mDatePicker;
+    private TimePicker mTimePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
         hideSystemUI(getWindow().getDecorView());
-//        startView = findViewById(R.id.weight_start);
-//        stopView = findViewById(R.id.weight_stop);
-//        mTextView = findViewById(R.id.weight_textview);
-//
-//        bootTag = getIntent().getIntExtra(WelcomeActivity.CONST_WEIGHT_TAG, WelcomeActivity.WEIGHT_INVALIDE);
-//        startTest();
-//        Log.i(TAG, "onCreate bootTag: " + bootTag);
+
+        mDatePicker = findViewById(R.id.datepicker);
+        mTimePicker = findViewById(R.id.timepicker);
     }
 
     // This snippet hides the system bars.
@@ -60,94 +44,36 @@ public class TimeActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_IMMERSIVE);
     }
 
-    /**
-     * 开始
-     */
-    float weight = 0;
-    private void startTest() {
-        startView.setVisibility(View.VISIBLE);
-        stopView.setVisibility(View.GONE);
-        mTextView.setText("");
-
-        // star phread
-        new Thread() {
-            @Override
-            public void run() {
-                while(true) {
-                    try {
-                        sleep(1);
-                        mHandler.sendEmptyMessage(WEIGHT_ACTIVITY);
-                        weight += 0.1;
-                        if (weight >= DEFAULT_WEIGHT) {
-                            mHandler.sendEmptyMessage(WEIGHT_STOP);
-                            break;
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }.start();
-    }
-
-    private Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what){
-                case WEIGHT_ACTIVITY:
-                    mTextView.setText("" + String.format(FORMAT_WEIGHT, weight));
-                    break;
-                case WEIGHT_STOP:
-                    stopTest();
-                    break;
-            }
-        }
-    };
-
-    /**
-     * 开始
-     */
-    private void stopTest() {
-        startView.setVisibility(View.GONE);
-        stopView.setVisibility(View.VISIBLE);
-    }
-
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.back_button:
-                finish();
-                break;
-            case R.id.next_button:
-                WelcomeActivity.getRecord().setWeight(Float.valueOf(mTextView.getText().toString()));
-                switch (bootTag) {
-                    case WEIGHT_VIP_TEST:
-                        startTouchId(this);
-                        break;
-                    case WEIGHT_NEW_TEST:
-                        WelcomeActivity.doTmpTest(this);
-                        break;
-                }
+            case R.id.no_button:
 //                finish();
                 break;
-        }
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        switch (requestCode) {
-            case REQUEST_CODE_TOUCHID:
-                if (resultCode == RESULT_OK) {
-                    int fingerId = intent.getIntExtra(CONST_FINGER_ID, INVALID_FINGER_ID);
-                    Person p = PersonBean.getInstance(this).queryByFingerId(fingerId);
-                    if (p != null) {
-                        WelcomeActivity.getRecord().setPerson(p);
-                        doVipTest(this);
-                        finish();
-                    } else {
-                        Log.i(TAG, "指纹识别异常");
-                    }
+            case R.id.yes_button:
+                int year = mDatePicker.getYear();
+                int month = mDatePicker.getMonth();
+                int day = mDatePicker.getDayOfMonth();
+                int hour = mTimePicker.getCurrentHour();
+                int minute = mTimePicker.getCurrentMinute();
+
+                Calendar c = Calendar.getInstance();
+                c.set(Calendar.YEAR, mDatePicker.getYear());
+                c.set(Calendar.MONTH, mDatePicker.getMonth());
+                c.set(Calendar.DAY_OF_MONTH, mDatePicker.getDayOfMonth());
+                c.set(Calendar.HOUR, mTimePicker.getCurrentHour());
+                c.set(Calendar.MINUTE, mTimePicker.getCurrentMinute());
+                long when = c.getTimeInMillis();
+                if (when / 1000 < Integer.MAX_VALUE) {
+                    boolean ret = SystemClock.setCurrentTimeMillis(when);
+                    String text = "设置时间: " + ret;
+                    Log.i(TAG, text);
+                    Toast.makeText(this, text, Toast.LENGTH_LONG).show();
                 }
+
+                String text = String.valueOf(year) + "-" + String.valueOf(month) + "-" + String.valueOf(day) + "-" + String.valueOf(hour) + "-" + String.valueOf(minute);
+                Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+//                finish();
                 break;
         }
     }
