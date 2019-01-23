046d590211996e324ddc71aaa47b176c1761ac4c
kangear <kangear@163.com>
2019-01-23 22:47:37 +0800

    PDF排版

--- app/src/main/java/com/kangear/bodycompositionanalyzer/PdfActivity.java
+++ app/src/main/java/com/kangear/bodycompositionanalyzer/PdfActivity.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.databinding.DataBindingUtil;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
@@ -22,6 +23,9 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.kangear.bodycompositionanalyzer.databinding.ActivityResultBinding;
+import com.kangear.bodycompositionanalyzer.databinding.DialogPrintBinding;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -94,9 +98,50 @@ public class PdfActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dialog_print);
+        DialogPrintBinding binding = DataBindingUtil.setContentView(this, R.layout.dialog_print);
+//        setContentView(R.layout.dialog_print);
         hideSystemUI(getWindow().getDecorView());
         mContext = this;
+        byte[] data2 = {
+                /*0x55, (byte)0xAA, (byte)0xCD, 0x02, 0x31, 0x00, */0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x01, 0x28, (byte)0xA4, 0x06, 0x20, 0x03, (byte)0x9D, 0x1A, 0x7D, 0x16,
+                0x50, 0x00, 0x74, 0x0B, 0x48, 0x0B, 0x65, 0x01, (byte)0xF9, 0x09, 0x78, 0x09,
+                0x13, 0x15, 0x53, 0x13, 0x00, 0x00, 0x00, 0x00, 0x00, 0x7B, 0x02, 0x16,
+                0x02,(byte) 0xB5, 0x02, 0x33, 0x02, (byte)0xFC, 0x01, 0x1B, 0x02, (byte)0xED, 0x00, 0x5F,
+                0x00, 0x7F, 0x00, 0x10, 0x02, (byte)0xD5, 0x01, (byte)0xF5, 0x01, 0x2F, 0x01, 0x0A,
+                0x01, 0x4A, 0x01, (byte)0x99, 0x01, 0x6D, 0x01, (byte)0x84, 0x01, 0x77, 0x00, 0x64,
+                0x00, 0x71, 0x00, 0x23, 0x24, 0x26, 0x00, (byte)0x81, 0x00, (byte)0x8F, 0x00, (byte)0xA3,
+                0x00, 0x18, 0x01, (byte)0xF5, 0x00, 0x09, 0x01, 0x23, 0x1F, 0x21, 0x24, 0x1F,
+                0x21, 0x19, 0x01, (byte)0xEA, 0x00, (byte)0xF9, 0x00, 0x56, 0x00, 0x56, 0x00, 0x5C,
+                0x00, 0x5A, 0x00, 0x56, 0x00, 0x5C, 0x00, 0x0F, 0x06, 0x08, 0x0E, 0x06,
+                0x08, (byte)0x8C, 0x00, 0x2F, 0x00, 0x3F, 0x00, 0x23, 0x11, 0x17, 0x21, 0x11,
+                0x17, (byte)0xEB, 0x04, (byte)0x84, 0x03, 0x4C, 0x04, 0x14, 0x01, (byte)0xB9, 0x00, (byte)0xF0,
+                0x00, 0x28, 0x01, (byte)0x96, 0x00, (byte)0xC8, 0x00, 0x40, 0x04, 0x3E, 0x04, 0x5D,
+                0x04, (byte)0xB1, 0x03, 0x5D, 0x50, 0x5A, 0x1F, 0x1E, 0x23, 0x03, 0x04, (byte)0xF0,
+                0x04, (byte)0xA4, 0x02, 0x2B, (byte)0xA5, (byte)0x80, 0x7E, (byte)0x80, 0x2B, (byte)0x80, 0x0F, 0x07,
+                (byte)0xDC, 0x0A, (byte)0x81, (byte)0xA8
+        };
+
+        Record mRecord = new Record(0, 0, "xiaoguo2", 343, 1, "xxxx", 323, 2222);
+        mRecord.setData(data2);
+        mRecord.getBodyComposition();
+
+        binding.setRecord(mRecord);
+
+        Pdf pdf = new Pdf(mRecord, findViewById(R.id.pdf), this, null);
+
+        LocalBroadcastManager.getInstance(mContext).registerReceiver(mBroadcastReceiver, mPrintDoneFilter);
+
+        Message msg = new Message();
+        msg.what = HANDLE_EVENT_PRINT;
+        msg.obj = pdf;
+        mHandler.sendMessageDelayed(msg, 1);
+
+        if (true)
+            return;
+
+
         mFailView = findViewById(R.id.print_fail_view);
         mPrintingView = findViewById(R.id.printing_view);
 
@@ -150,12 +195,12 @@ public class PdfActivity extends AppCompatActivity {
         Log.i(TAG, "mRecords: " + records.size());
         Toast.makeText(this, "历史记录数:"+records.size(), Toast.LENGTH_LONG).show();
 
-        Pdf pdf = new Pdf(curRecord, findViewById(R.id.pdf), this, records);
-
-        Message msg = new Message();
-        msg.what = HANDLE_EVENT_PRE_FILL;
-        msg.obj = pdf;
-        mHandler.sendMessageDelayed(msg, 1);
+//        Pdf pdf = new Pdf(curRecord, findViewById(R.id.pdf), this, records);
+//
+//        Message msg = new Message();
+//        msg.what = HANDLE_EVENT_PRINT;
+//        msg.obj = pdf;
+//        mHandler.sendMessageDelayed(msg, 1);
 
         LocalBroadcastManager.getInstance(mContext).registerReceiver(mBroadcastReceiver, mPrintDoneFilter);
     }
