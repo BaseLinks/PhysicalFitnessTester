63346ea5983863f09415b5ef017a6c6c1290d5f8
tony <kangear@163.com>
2019-08-15 11:11:49 +0800

    同步解决jh303的问题

--- app/src/jh303plus/java/com/kangear/bodycompositionanalyzer/PdfActivity.java
+++ app/src/jh303plus/java/com/kangear/bodycompositionanalyzer/PdfActivity.java
@@ -31,6 +31,7 @@ import android.widget.Toast;
 
 import com.kangear.bodycompositionanalyzer.databinding.ActivityResultBinding;
 import com.kangear.bodycompositionanalyzer.databinding.DialogPrintBinding;
+import com.kangear.bodycompositionanalyzer.entry.SchoopiaRecord;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -292,8 +293,10 @@ public class PdfActivity extends BaseActivity {
                         @Override
                         public void run() {
                             super.run();
+                            mSchList.clear();
                             for (Record r : pdf.getRecords()) {
                                 r.getBodyComposition();
+                                mSchList.add(SchoopiaRecord.toHere(r));
                             }
                             Message msg = new Message();
                             msg.what = HANDLE_EVENT_FILL;
@@ -352,88 +355,7 @@ public class PdfActivity extends BaseActivity {
         }
     }
 
-    // 单个框
-    private static void fillOne(View v, final BodyComposition.Third t, final int textviewId,
-                 final boolean isNeedUnit, final String format) {
-        String text = String.format(format, t.getCur());
-        if (isNeedUnit)
-            text += t.getUnit();
-        ((TextView)v.findViewById(textviewId)).setText(text);
-    }
-
-    private static void fillRange(View pdfView, final BodyComposition.Third t, final int textviewId,
-                 final boolean isNeedUnit, final String format) {
-        String text = String.format(format, t.getMin()) + "-" + String.format(format, t.getMax());
-        ((TextView)pdfView.findViewById(textviewId)).setText(text);
-    }
-
-    private static void fillCurAndRange(View pdfView, final BodyComposition.Third t, final int textviewId,
-                         final boolean isNeedUnit, final String format) {
-        String RANGE_FORMAT = "(%.1f-%.1f)";
-        String cur = String.format("%.1f", t.getCur()) + t.getUnit();
-        String text = cur + String.format(RANGE_FORMAT, t.getMin(), t.getMax());
-        ((TextView)pdfView.findViewById(textviewId)).setText(text);
-    }
-
-    // 肥胖分析
-    private static void fillCurAndRange(View pdfView, final BodyComposition.Third t, final int curId,
-                         final int rangeId,
-                         final boolean isNeedUnit, final String format) {
-        fillOne(pdfView, t, curId, isNeedUnit, format);
-        fillRange(pdfView, t, rangeId, isNeedUnit, format);
-    }
-
-    //
-    private static void fillYelloMan(ResultActivity.YelloMan yelloMan, View ym) {
-        if (yelloMan == null || ym == null)
-            return;
-
-        ((TextView) ym.findViewById(R.id.la_level_textview)).setText(yelloMan.getLeftArmLevel());
-        ((TextView) ym.findViewById(R.id.ll_level_textview)).setText(yelloMan.getLeftLegeLevel());
-        ((TextView) ym.findViewById(R.id.qugan_level_textview)).setText(yelloMan.getQuganLevel());
-        ((TextView) ym.findViewById(R.id.ra_level_textview)).setText(yelloMan.getRightArmLevel());
-        ((TextView) ym.findViewById(R.id.rl_level_textview)).setText(yelloMan.getRightLegeLevel());
-
-        ((TextView) ym.findViewById(R.id.la_textview)).setText(yelloMan.getLeftArm());
-        ((TextView) ym.findViewById(R.id.ll_textview)).setText(yelloMan.getLeftLege());
-        ((TextView) ym.findViewById(R.id.qugan_textview)).setText(yelloMan.getQugan());
-        ((TextView) ym.findViewById(R.id.ra_textview)).setText(yelloMan.getRightArm());
-        ((TextView) ym.findViewById(R.id.rl_textview)).setText(yelloMan.getRightLege());
-    }
-
-    private void fillYingYangPingGu(int curId, int progressid, BodyComposition.Third t) {
-        TextView etCur = findViewById(curId);
-        ProgressBar pb = findViewById(progressid);
-        etCur.setText(String.format(FLOAT_1_FORMAT, t.getCur()) + t.getUnit());
-        pb.setProgress(t.getProgress(YINGYANGPINGGU_LESS_WIDTH, YINGYANGPINGGU_NOMAL_WIDTH, YINGYANGPINGGU_MORE_WIDTH));
-    }
-
-
-    private static void setProgressOfTichengfenfenxi(BodyComposition.Third t, View view) {
-        final int PECENT_MAX = 100;
-        final int HUMAN_HIGH = 208; // 208px
-        final float TWO_GE = (float) (20.8 * 2);
-        final float BILI = HUMAN_HIGH / PECENT_MAX;
-        final int progress = t.getProgress(TICHENGFENFENXI_LESS_WIDTH, TICHENGFENFENXI_NOMAL_WIDTH, TICHENGFENFENXI_MORE_WIDTH);
-        final ImageView progressView = view.findViewById(R.id.shentichengfenfenxi_frontgound_imageview);
-        final TextView textView = view.findViewById(R.id.progress_textview);
-        progressView.setVisibility(View.VISIBLE);
-        progressView.getLayoutParams().width = (int) (((PECENT_MAX - progress) * BILI) + TWO_GE);
-        progressView.requestLayout();
-        textView.setText(String.format(FORMAT_WEIGHT, t.getCur()) + t.getUnit());
-    }
-
-    private static void setProgressOfYingYangPingGu(BodyComposition.Third t, View view) {
-        final int PECENT_MAX = 100;
-        final float total = YINGYANGPINGGU_LESS_WIDTH + YINGYANGPINGGU_NOMAL_WIDTH + YINGYANGPINGGU_MORE_WIDTH;
-        final float BILI = total / PECENT_MAX;
-        final int progress = t.getProgress(YINGYANGPINGGU_LESS_WIDTH, YINGYANGPINGGU_NOMAL_WIDTH, YINGYANGPINGGU_MORE_WIDTH);
-        final View progressView = view.findViewById(R.id.progress);
-        progressView.setVisibility(View.VISIBLE);
-        progressView.getLayoutParams().width = (int) ((PECENT_MAX - progress) * BILI);
-        progressView.requestLayout();
-    }
-
+    private List<SchoopiaRecord> mSchList = new ArrayList<>();
 
     private void fillPdfView(Pdf pdf) {
         if (pdf == null) {
@@ -471,7 +393,7 @@ public class PdfActivity extends BaseActivity {
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        RecyclerView.Adapter mAdapter = new RecordPdfAdapter(mRecords);
+        RecyclerView.Adapter mAdapter = new RecordPdfAdapter(mSchList);
         mRecyclerView.setAdapter(mAdapter);
 
         // Company and Number
@@ -481,99 +403,5 @@ public class PdfActivity extends BaseActivity {
         String n = o2 == null ? "" : o2.getStrValue();
         binding.setCompany(c);
         binding.setNumber(n);
-//        Toast.makeText(activity, "company: " + c + " number: " + n, Toast.LENGTH_LONG).show();
-//        ((TextView)pdfView.findViewById(R.id.company_textview)).setText("地址: "+ c + "  " + n);
-//        ((TextView)pdfView.findViewById(R.id.number_textview)).setText(n);
-//
-//        // JiBenXinXi
-//        // - ID, Date, Time, Gender
-//        ((TextView)pdfView.findViewById(R.id.name_textview)).setText(curRecord.getName());
-//        ((TextView)pdfView.findViewById(R.id.gender_textview)).setText(curRecord.getGender());
-//        Date date = new Date(curRecord.getTime());
-//        ((TextView)pdfView.findViewById(R.id.jibenxinxi_date_textview)).setText(new SimpleDateFormat(JIBENXINXI_DATE_FORMAT).format(date));
-//        ((TextView)pdfView.findViewById(R.id.time_textview)).setText(new SimpleDateFormat(JIBENXINXI_TIME_FORMAT).format(date));
-//
-//        //
-//        fillOne(pdfView, bc.身高, R.id.height_textview, true, FORMAT_WEIGHT);
-//        fillOne(pdfView, bc.年龄, R.id.age_textview, true, FLOAT_0_FORMAT);
-//        fillOne(pdfView, bc.评分, R.id.jibenxinxi_jiankangzhishu_textview, false, FLOAT_1_FORMAT);
-//
-//        // 身体成分分析
-//        fillRange(pdfView, bc.体重, R.id.weight_range_textview, false, FLOAT_1_FORMAT);
-//        fillRange(pdfView, bc.骨骼肌, R.id.gugeji_range_textview, false, FLOAT_1_FORMAT);
-//        fillRange(pdfView, bc.体脂肪量, R.id.tizhifang_range_textview, false, FLOAT_1_FORMAT);
-//        setProgressOfTichengfenfenxi(bc.体重, pdfView.findViewById(R.id.weight_progressbar));
-//        setProgressOfTichengfenfenxi(bc.骨骼肌, pdfView.findViewById(R.id.gugeji_progressbar));
-//        setProgressOfTichengfenfenxi(bc.体脂肪量, pdfView.findViewById(R.id.tizhifang_progressbar));
-//
-//        // -身体水分
-//        fillCurAndRange(pdfView, bc.身体水分, R.id.shentishuifen_textview, true, FLOAT_1_FORMAT);
-//        // -去脂体重
-//        fillCurAndRange(pdfView, bc.去脂体重, R.id.quzhitizhong_textview, true, FLOAT_1_FORMAT);
-//
-//        // 营养评估
-//        // -蛋白质 无机盐 总能耗
-//        fillOne(pdfView, bc.蛋白质, R.id.danbaizhi_textview, true, FLOAT_1_FORMAT);
-//        fillOne(pdfView, bc.无机盐, R.id.wujiyan_textview, true, FLOAT_1_FORMAT);
-//        fillOne(pdfView, bc.总能耗, R.id.zongnenghao_textview, false, FLOAT_0_FORMAT);
-//        setProgressOfYingYangPingGu(bc.蛋白质, pdfView.findViewById(R.id.danbaizhi_progressbar));
-//        setProgressOfYingYangPingGu(bc.无机盐, pdfView.findViewById(R.id.wujiyan_progressbar));
-//
-//        // 肥胖分析
-//        fillCurAndRange(pdfView, bc.BMI, R.id.bmi_textview, R.id.bmi_range_textview, true, FLOAT_1_FORMAT);
-//        fillCurAndRange(pdfView, bc.体脂百分比, R.id.pdf_tizhibaifenbi_textview, R.id.tizhibaifenbi_range_textview, true, FLOAT_1_FORMAT);
-//        fillCurAndRange(pdfView, bc.腰臀比, R.id.yaotunbi_textview, R.id.yaotunbi_range_textview, true, FLOAT_2_FORMAT);
-//        fillOne(pdfView, bc.基础代谢, R.id.jichudaixie_textview, true, FLOAT_0_FORMAT);
-//
-//        // zonghepingjia
-//        // -内脏面积
-//        fillCurAndRange(pdfView, bc.内脏面积, R.id.neizangmianji_textview, true, FLOAT_1_FORMAT);
-//
-//        // -阶段
-//        // - 阶段脂肪
-//        fillYelloMan(
-//                new ResultActivity.YelloMan(
-//                        null,
-//                        bc.左上脂肪量,
-//                        bc.左下脂肪量,
-//                        bc.右上脂肪量,
-//                        bc.右下脂肪量,
-//                        bc.躯干脂肪量
-//                ),
-//                pdfView.findViewById(R.id.zhifang_yellow_human)
-//
-//        );
-//
-//        // - 阶段肌肉
-//        fillYelloMan(
-//                new ResultActivity.YelloMan(
-//                        null,
-//                        bc.左上肢肌肉量,
-//                        bc.左下肌肉量,
-//                        bc.右上肢肌肉量,
-//                        bc.右下肌肉量,
-//                        bc.躯干肌肉量
-//                ),
-//                pdfView.findViewById(R.id.jirou_yellow_human)
-//
-//        );
-//
-//        // 综合评价
-//        FLOAT_ZHIFANG_TIAOZHENGLIANG_FORMAT = "-" + FLOAT_1_FORMAT + bc.体脂肪量.getUnit();
-//        FLOAT_JIROU_TIAOZHENGLIANG_FORMAT   = "+" + FLOAT_1_FORMAT + bc.骨骼肌.getUnit();
-//        // -脂肪调整量
-//        ((TextView)pdfView.findViewById(R.id.zhifangtiaozheng_textview)).setText(String.format(FLOAT_ZHIFANG_TIAOZHENGLIANG_FORMAT, bc.getZhifangAdjustment()));
-//        // -肌肉调整量
-//        ((TextView)pdfView.findViewById(R.id.jiroutiaozheng_textview)).setText(String.format(FLOAT_JIROU_TIAOZHENGLIANG_FORMAT, bc.getJirouAdjustment()));
-//        // -健康指数(评分)
-//        fillOne(pdfView, bc.评分, R.id.jiankangzhishu_textview, false, FLOAT_1_FORMAT);
-//
-//        // 生物电阻抗
-//        // -5k
-//        fillOne(pdfView, bc._5k电阻, R.id._5k_textview, true, FLOAT_0_FORMAT);
-//        // -50k
-//        fillOne(pdfView, bc._50k电阻, R.id._50k_textview, true, FLOAT_0_FORMAT);
-//        // -250k
-//        fillOne(pdfView, bc._250k电阻, R.id._250k_textview, true, FLOAT_0_FORMAT);
     }
 }
